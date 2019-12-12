7f46ad0b0b29ccaa24f69620c5f32cd1db6ef8f7
Sanchez <skrolsky@gmail.com>
2019-12-12 11:29:52 +0300

    add build

--- main.tf
+++ main.tf
@@ -6,3 +6,14 @@ resource "heroku_app" "default" {
   name   = "mr-gekko"
   region = "us"
 }
+
+# Build code & release to the app
+resource "heroku_build" "default" {
+  app = "${heroku_app.default.name}"
+  buildpacks = ["https://github.com/HoldemGK/gekko.git"]
+
+  source = {
+    url = "https://github.com/HoldemGK/gekko/archive/work-stable.zip"
+    
+  }
+}
